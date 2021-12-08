52a26101e0fed40993577346ec8fdadca2703e2c
Justin Douglas <tabidots@gmail.com>
2021-12-08 00:56:37 -0600

    Finished Day 8 in Clojure

--- py/day05.py
+++ py/day05.py
@@ -17,10 +17,6 @@ def inclusive_range(a, b):
 class Line:
     def __init__(self, x1, y1, x2, y2):
         self.is_diagonal = x1 != x2 and y1 != y2
-        self.x1 = x1
-        self.x2 = x2
-        self.y1 = y1
-        self.y2 = y2
         self.points = self.draw(x1, y1, x2, y2)
 
     def draw(self, x1, y1, x2, y2):
---
+++ src/aoc2021/day08.clj
@@ -0,0 +1,71 @@
+(ns aoc2021.day08
+  (:require [clojure.java.io :as io]
+            [clojure.string :as s]
+            [clojure.set :as set]))
+
+(defn parse
+  [data]
+  (for [line data]
+    (let [[signal-patterns output-values] (s/split line #" \| ")]
+      {:signal-patterns (->> (s/split signal-patterns #" ")
+                             (map (comp set s/join sort)))
+       :output-values   (->> (s/split output-values #" ")
+                             (map (comp set s/join sort)))})))
+
+(def small-sample-input
+  (-> "../resources/day08_ex_small.txt" io/resource io/reader line-seq parse))
+
+(def sample-input
+  (-> "../resources/day08_ex.txt" io/resource io/reader line-seq parse))
+
+(def puzzle-input
+  (-> "../resources/day08.txt" io/resource io/reader line-seq parse))
+
+;; "1" = 2 segments, "4" = 4 segments, "7" = 3 segments, "8" = 7 segments
+
+(defn part-1
+  [data]
+  (->> (map :output-values data)
+       (reduce into [])
+       (keep #(get #{2 3 4 7} (count %)))
+       (count)))
+
+(defn get-by-segments
+  [num-segments patterns]
+  (filter #(= (count %) num-segments) patterns))
+
+;; 6 segments = "0,6,9"
+;; 5 segments = "2,3,5"
+
+(defn decode-patterns
+  [patterns]
+  (let [num-one    (first (get-by-segments 2 patterns))
+        num-seven  (first (get-by-segments 3 patterns))
+        num-four   (first (get-by-segments 4 patterns))
+        num-eight  (first (get-by-segments 7 patterns))
+        fivers     (get-by-segments 5 patterns)
+        sixers     (get-by-segments 6 patterns)
+        ;; "9" is the only sixer that is a superset of "4"
+        num-nine   (first (filter (partial set/subset? num-four) sixers))
+        ;; "0" is the only remaining sixer that is a superset of "1"
+        num-zero   (->> (remove #{num-nine} sixers)
+                        (filter (partial set/subset? num-one))
+                        first)
+        num-six    (first (remove #{num-nine num-zero} sixers))
+        ;; "3" is the only fiver that is a superset of "1"
+        num-three  (first (filter (partial set/subset? num-one) fivers))
+        ;; "5" is the only fiver that is a subset of "6"
+        num-five   (first (filter #(set/subset? % num-six) fivers))
+        num-two    (first (remove #{num-five num-three} fivers))]
+    {num-zero "0" num-one "1" num-two "2" num-three "3" num-four "4"
+     num-five "5" num-six "6" num-seven "7" num-eight "8" num-nine "9"}))
+
+(defn part-2
+  [data]
+  (apply +
+    (for [line data]
+      (let [lut (decode-patterns (:signal-patterns line))]
+        (->> (:output-values line)
+             (map lut)
+             s/join
+             (Integer/parseInt))))))
